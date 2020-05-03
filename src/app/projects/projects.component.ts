dccd5cfd3642105b081e875fd2851fcdfb46ea43
tylercchase <tylercchase@gmail.com>
2020-05-03 11:04:16 -0800

    Data for projects

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,Input } from '@angular/core';
+import { RawSource } from 'webpack-sources';
 
 @Component({
   selector: 'app-projects',
   templateUrl: './projects.component.html',
   styleUrls: ['./projects.component.scss']
 })
-export class ProjectsComponent implements OnInit {
 
-  constructor() { }
+export class ProjectsComponent implements OnInit {
+  @Input() projects = [
+    {
+      title:"Pilet",
+      tags:['javascript',],
+      content: `An OpenSource plug used for helping prevent your car from freezing in the -40 weather of Alaska. Built by me and a team of people at a hackathon at the University of Alaska Fairbanks.`,
+    },
+    {
+      title:"Cool thang",
+      tags:['typescript']
+    }
+  ];
+
+  constructor() {
+   }
 
   ngOnInit() {
+
   }
 
 }
